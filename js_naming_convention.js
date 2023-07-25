f45584c1ba76baaaadd79966fb630be59e550661
yhj0318 <gudwns0318@naver.com>
2023-07-25 13:20:50 +0900

    first commit

---
+++ js_naming_convention.js
@@ -0,0 +1,21 @@
+a = 100;
+alert(a);
+alert(Math.round(a * Math.random()));
+
+/*
+1) var - 더이상 쓰지 않는다.
+2) let
+3) const 
+ */
+
+var name = '염형준';
+console.log(name);
+document.write(name + "<br>");
+
+let age = 23;
+console.log(age);
+document.write(age + "<br>");
+
+const grade = 3;
+document.write(grade);
+// grade = 1; const는 수정불가능 항상 초기화 해야함
